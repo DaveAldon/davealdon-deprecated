e883c0a0d87915b4def1500d9da13c4adb791292
David Crawford <crawford@mail.gvsu.edu>
2018-05-15 01:01:44 -0400

    Updated navigation and mobile javascript.

--- js/ajax-navigation.js
+++ js/ajax-navigation.js
@@ -4,6 +4,18 @@ function navigate(pageRef) {
 }
 
 jQuery(document).ready(function($) {
+	// Handler for mobile menu
+  $("#nav-mobile").html($("#nav-main").html());
+  $("#nav-trigger span").click(function(){
+      if ($("nav#nav-mobile ul").hasClass("expanded")) {
+          $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+          $(this).removeClass("open");
+      } else {
+          $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+          $(this).addClass("open");
+      }
+  });
+		
   // Default to homepage
   navigate("pages/home.html")
   // Click event for nav links to run ajax on content element
