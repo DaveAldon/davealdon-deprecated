64cb584f0345c8fddd42935f6c04f90a475d4833
David Crawford <crawford@mail.gvsu.edu>
2018-05-19 00:21:11 -0400

    Restructured SPA to work with nav/footer templates instead of all content. Individual pages have their own URLs now.

--- js/ajax-navigation.js
+++ js/ajax-navigation.js
@@ -1,27 +1,16 @@
-function navigate(pageRef) {
-	// AJAX shorthand for dumping html contents into element
-	$('#wrap').load(pageRef);
-}
-
 jQuery(document).ready(function($) {
+	// Template handling
+	$("#nav").load("../pages/nav.html");
+	$("#footer").load("../pages/footer.html");
 	// Handler for mobile menu
   $("#nav-mobile").html($("#nav-main").html());
   $("#nav-trigger span").click(function(){
-      if ($("nav#nav-mobile ul").hasClass("expanded")) {
-          $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+      if ($("nav-bar#nav-mobile ul").hasClass("expanded")) {
+          $("nav-bar#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
           $(this).removeClass("open");
       } else {
-          $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+          $("nav-bar#nav-mobile ul").addClass("expanded").slideDown(250);
           $(this).addClass("open");
       }
-  });
-
-  // Default to homepage
-  navigate("pages/home.html")
-  // Click event for nav links to run ajax on content element
-  $('.nav-link').on("click", function(e) {
-    e.preventDefault();
-		var pageRef = $(this).attr('href');
-		navigate(pageRef)
   });
 });
